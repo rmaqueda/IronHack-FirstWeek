d819264dba6d126f6431ea0e92b96883272a8a3e
Xavier Hernandez <xavier@xavierhernandez.com>
2014-06-02 17:30:04 +0200

    Initial Commit

---
+++ beers/beersAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  beersAppDelegate.h
+//  beers
+//
+//  Created by Xavier Hernandez on 02/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface beersAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ beersTests/beersTests.m
@@ -0,0 +1,34 @@
+//
+//  beersTests.m
+//  beersTests
+//
+//  Created by Xavier Hernandez on 02/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface beersTests : XCTestCase
+
+@end
+
+@implementation beersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
